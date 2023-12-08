06458b71c5644953f9f27a74c2137ea1ac2add1d
Punit Jain <contactpunitjain@gmail.com>
2023-12-08 16:06:15 +0530

    before defining tuple type

--- tsbasics/app.js
+++ tsbasics/app.js
@@ -1,10 +1,13 @@
 var person = {
     name: 'punit',
     age: 40,
-    hobbies: ['Sports', 'Cooking']
+    hobbies: ['Sports', 'Cooking'],
+    role: ['author']
 };
 console.log(person.name);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby.toUpperCase());
 }
+person.role.push('player');
+console.log(person.role);
--- tsbasics/app.ts
+++ tsbasics/app.ts
@@ -1,11 +1,13 @@
 const person:{
     name: string;
     age: number;
-    hobbies: string[]
+    hobbies: string[];
+    role: string[]
 } = {
     name: 'punit',
     age:40,
-    hobbies: ['Sports', 'Cooking']
+    hobbies: ['Sports', 'Cooking'],
+    role: ['author']
 }
 
 console.log(person.name)
@@ -13,3 +15,6 @@ console.log(person.name)
 for(const hobby of person.hobbies) {
     console.log(hobby.toUpperCase())
 }
+
+person.role.push('player')
+console.log(person.role)
