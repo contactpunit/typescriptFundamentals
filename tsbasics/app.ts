67aec9954c0aff089fc5671624a6f762650ea84f
Punit Jain <contactpunitjain@gmail.com>
2023-12-08 15:52:43 +0530

    array type

--- tsbasics/app.ts
+++ tsbasics/app.ts
@@ -1,9 +1,15 @@
 const person:{
     name: string;
-    age: number
+    age: number;
+    hobbies: string[]
 } = {
     name: 'punit',
-    age:40
+    age:40,
+    hobbies: ['Sports', 'Cooking']
 }
 
 console.log(person.name)
+
+for(const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase())
+}
