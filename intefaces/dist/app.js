d5f7b11b9a28e87bebb0834263d29831b7016a57
Punit Jain <contactpunitjain@gmail.com>
2023-12-09 14:39:49 +0530

    initialization shorthand

--- intefaces/dist/app.js
+++ intefaces/dist/app.js
@@ -1,8 +1,26 @@
 "use strict";
 class Department {
-    constructor(name) {
+    // private name: string;
+    // private employees: string[];
+    constructor(name, employees) {
         this.name = name;
+        this.employees = employees;
+        // this.name = name
+        // this.employees = []
+    }
+    describe() {
+        console.log('Department: ' + this.name);
+    }
+    addEmployee(employee) {
+        if (!this.employees.includes(employee))
+            this.employees.push(employee);
+    }
+    employeesInfo() {
+        console.log(`Employees afor department ${this.name} are ${this.employees.join(',')}`);
     }
 }
-const it = new Department('IT');
-console.log(it);
+const it = new Department('IT', []);
+it.describe();
+it.addEmployee('Punit');
+it.addEmployee('Manu');
+it.employeesInfo();
--- intefaces/src/app.ts
+++ intefaces/src/app.ts
@@ -1,9 +1,27 @@
 class Department {
-    name: string;
-    constructor(name: string) {
-        this.name = name
+    // private name: string;
+    // private employees: string[];
+
+    constructor(private name: string, private employees:string[]) {
+        // this.name = name
+        // this.employees = []
+    }
+
+    describe() {
+        console.log('Department: ' + this.name)
+    }
+
+    addEmployee(employee: string) {
+        if (!this.employees.includes(employee)) this.employees.push(employee)
+    }
+
+    employeesInfo() {
+        console.log(`Employees afor department ${this.name} are ${this.employees.join(',')}`)
     }
 }
 
-const it = new Department('IT')
-console.log(it)
+const it = new Department('IT', [])
+it.describe()
+it.addEmployee('Punit')
+it.addEmployee('Manu')
+it.employeesInfo()
