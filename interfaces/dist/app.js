f5b66e576255fe675df1c60ea2c36970c899c0c7
Punit Jain <contactpunitjain@gmail.com>
2023-12-10 11:42:27 +0530

    extnding multiple interfaces

--- interfaces/dist/app.js
+++ interfaces/dist/app.js
@@ -1,12 +1,20 @@
 "use strict";
 class Employee {
-    constructor(name, age) {
+    constructor(name, age, deptname) {
         this.name = name;
         this.age = age;
+        Employee.nextId += 1;
+        this.id = Employee.nextId;
+        this.deptName = deptname;
+        this.deptId = this.deptName === 'IT' ? 1 : 2;
     }
     greet(message) {
         console.log(`${message} ${this.name}`);
     }
 }
-const user = new Employee('Punit', 33);
-user.greet('Good morning');
+Employee.nextId = 0;
+const user1 = new Employee('Punit', 33, 'IT');
+const user2 = new Employee('Nidhi', 34, 'Finance');
+const user3 = new Employee('Anki', 34, 'Finance');
+user1.greet('Good morning');
+console.log(user3);
--- interfaces/src/app.ts
+++ interfaces/src/app.ts
@@ -1,16 +1,30 @@
 interface Person {
-    name: string;
+    readonly name: string;
+    readonly id: number
     age: number;
     greet(message: string): void;
 }
 
-class Employee implements Person {
-    name: string;
-    age: number;
+interface Department {
+    readonly deptName: string
+    readonly deptId: number
+}
 
-    constructor(name: string, age: number) {
+class Employee implements Person, Department {
+    name: string;
+    id: number
+    age: number;
+    deptName: string;
+    deptId: number
+    static nextId: number = 0
+
+    constructor(name: string, age: number, deptname: string) {
         this.name = name
         this.age = age
+        Employee.nextId += 1
+        this.id = Employee.nextId
+        this.deptName = deptname
+        this.deptId = this.deptName === 'IT' ? 1 : 2
     }
 
     greet(message: string) {
@@ -18,5 +32,8 @@ class Employee implements Person {
     }
 }
 
-const user: Person = new Employee('Punit', 33)
-user.greet('Good morning')
+const user1: Person = new Employee('Punit', 33, 'IT')
+const user2: Person = new Employee('Nidhi', 34, 'Finance')
+const user3: Person = new Employee('Anki', 34, 'Finance')
+user1.greet('Good morning')
+console.log(user3)
