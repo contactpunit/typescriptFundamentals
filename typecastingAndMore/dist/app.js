6ad65e9c5b5fb02c45946f40c5569a57128b89c9
Punit Jain <contactpunitjain@gmail.com>
2023-12-10 21:14:09 +0530

    index properties for interfaces

--- typecastingAndMore/dist/app.js
+++ typecastingAndMore/dist/app.js
@@ -3,3 +3,8 @@ const inputBox = document.getElementById('username');
 if (inputBox) {
     inputBox.value = 'hello there';
 }
+const user1 = {
+    userName: 'punit',
+    userEmail: 'pjain@gmail.com',
+    address: 'India'
+};
--- typecastingAndMore/src/app.ts
+++ typecastingAndMore/src/app.ts
@@ -2,3 +2,15 @@ const inputBox = document.getElementById('username')
 if(inputBox) {
     (inputBox as HTMLInputElement).value = 'hello there'
 }
+
+// index properties
+
+interface UserDetail {
+    [prop: string]: string
+}
+
+const user1: UserDetail = {
+    userName: 'punit',
+    userEmail: 'pjain@gmail.com',
+    address: 'India'
+}
