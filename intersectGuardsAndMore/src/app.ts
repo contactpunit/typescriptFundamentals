62b7d22054ecfeb5bb767d4fb7fd7fde302fd09a
Punit Jain <contactpunitjain@gmail.com>
2023-12-10 15:01:44 +0530

    type guards

--- intersectGuardsAndMore/src/app.ts
+++ intersectGuardsAndMore/src/app.ts
@@ -1,3 +1,17 @@
+type Numeric = number
+type AlphaNumeric = number | string
+
+type Combined = Numeric | AlphaNumeric
+
+function addNums(a: Combined, b: Combined) {  // apply type guards
+    if(typeof a === 'string' || typeof b === 'string') {
+        return a.toString() + b.toString()
+    }
+    return a + b
+}
+
+// ******************
+
 type Admin = {
     name: string;
     department: string;
@@ -8,10 +22,21 @@ type Manager = {
     priviledges: string[]
 }
 
-type Employee = Admin & Manager
+type Employee = Admin | Manager
 
 const user1: Employee = {
     name: 'Punit',
-    department: 'IT',
-    priviledges: ['restartServer']
+    department: 'IT'
 }
+
+function printEmployeeInfo(employee: Employee) {
+    console.log(employee.name)
+    if('department' in employee) {  // applying type guards here
+        console.log(employee.department)
+    }
+    if('priviledges' in employee) {  // applying type guards here
+        console.log(employee.priviledges)
+    }
+}
+
+
