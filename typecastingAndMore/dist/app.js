0b9a6150cde5468a7e4eee2f6b65a9810ea8b38c
Punit Jain <contactpunitjain@gmail.com>
2023-12-10 16:52:30 +0530

    type casting

---
+++ typecastingAndMore/dist/app.js
@@ -0,0 +1,5 @@
+"use strict";
+const inputBox = document.getElementById('username');
+if (inputBox) {
+    inputBox.value = 'hello there';
+}
---
+++ typecastingAndMore/src/app.ts
@@ -0,0 +1,4 @@
+const inputBox = document.getElementById('username')
+if(inputBox) {
+    (inputBox as HTMLInputElement).value = 'hello there'
+}
