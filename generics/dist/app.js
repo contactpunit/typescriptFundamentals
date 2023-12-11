1cd70dc743c5be6c7268503075248eed2a400c82
Punit Jain <contactpunitjain@gmail.com>
2023-12-11 14:02:17 +0530

    generic classes

--- generics/dist/app.js
+++ generics/dist/app.js
@@ -25,3 +25,23 @@ console.log(countanddescribe(['Punit', 'Jain']));
 function extractAndConvert(obj, key) {
     return obj[key];
 }
+// generic classes
+class Sweets {
+    constructor() {
+        this.allSweets = [];
+    }
+    addSweet(item) {
+        this.allSweets.push(item);
+    }
+    removeSweet(item) {
+        this.allSweets.splice(this.allSweets.indexOf(item), 1);
+    }
+    getSweets() {
+        return `All sweets are: ${this.allSweets.join(',')}`;
+    }
+}
+const sweet1 = new Sweets();
+sweet1.addSweet('rasgulla');
+sweet1.addSweet('rasmalai');
+sweet1.addSweet('gulab jamun');
+console.log(sweet1.getSweets());
--- generics/src/app.ts
+++ generics/src/app.ts
@@ -38,3 +38,27 @@ console.log(countanddescribe(['Punit', 'Jain']))
 function extractAndConvert<T extends object, U extends keyof T>(obj: T, key: U) {
     return obj[key]
 }
+
+// generic classes
+
+class Sweets<T extends string|number|boolean> {
+    private allSweets: T[] = []
+
+    addSweet(item: T) {
+        this.allSweets.push(item)
+    }
+
+    removeSweet(item: T) {
+        this.allSweets.splice(this.allSweets.indexOf(item), 1)
+    }
+
+    getSweets() {
+        return `All sweets are: ${this.allSweets.join(',')}`
+    }
+}
+
+const sweet1 = new Sweets<string>()
+sweet1.addSweet('rasgulla')
+sweet1.addSweet('rasmalai')
+sweet1.addSweet('gulab jamun')
+console.log(sweet1.getSweets())
