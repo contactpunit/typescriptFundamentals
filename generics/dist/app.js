cddc71544d46144d070d996a6b09e9854918d978
Punit Jain <contactpunitjain@gmail.com>
2023-12-11 13:29:00 +0530

    keyof for checking key for an generic object

--- generics/dist/app.js
+++ generics/dist/app.js
@@ -10,3 +10,18 @@ function merge(obj1, obj2) {
     return Object.assign(obj3, obj1, obj2);
 }
 console.log(merge(obj1, obj2));
+function countanddescribe(elem) {
+    let description = '';
+    if (elem.length === 0) {
+        description = 'Array of 0 elements';
+    }
+    else {
+        description = `Array elem has ${elem.length} elements`;
+    }
+    return [elem, description];
+}
+console.log(countanddescribe('Hello Punit'));
+console.log(countanddescribe(['Punit', 'Jain']));
+function extractAndConvert(obj, key) {
+    return obj[key];
+}
--- generics/src/app.ts
+++ generics/src/app.ts
@@ -14,3 +14,27 @@ function merge<T extends object, U extends object>(obj1: T, obj2: U) {
 }
 
 console.log(merge(obj1, obj2))
+
+
+// function that takes an input of generic type and return a tuple of element itself and decription about element
+
+interface Elements {
+    length: number;
+}
+
+function countanddescribe<T extends Elements>(elem: T): [T, string] {
+    let description: string = ''
+    if(elem.length === 0) {
+        description = 'Array of 0 elements'
+    } else {
+        description = `Array elem has ${elem.length} elements`
+    }
+    return [elem, description]
+}
+
+console.log(countanddescribe('Hello Punit'))
+console.log(countanddescribe(['Punit', 'Jain']))
+
+function extractAndConvert<T extends object, U extends keyof T>(obj: T, key: U) {
+    return obj[key]
+}
