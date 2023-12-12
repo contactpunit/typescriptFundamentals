e3d95acf2fb3243017468bde29cdf005aa2b8294
Punit Jain <contactpunitjain@gmail.com>
2023-12-12 16:13:51 +0530

    render form template

--- DragDropProject/src/app.ts
+++ DragDropProject/src/app.ts
@@ -1,1 +1,21 @@
 // Code goes here!
+class Project {
+    templateFormEl: HTMLTemplateElement;
+    templateDestinationEl: HTMLDivElement;
+    formElement: HTMLFormElement
+
+    constructor() {
+        this.templateFormEl = document.getElementById('project-input')! as HTMLTemplateElement
+        this.templateDestinationEl =document.getElementById('app')! as HTMLDivElement
+
+        const importedNode = document.importNode(this.templateFormEl.content, true)
+        this.formElement = importedNode.firstElementChild as HTMLFormElement
+        this.render()
+    }
+
+    render() {
+        this.templateDestinationEl.insertAdjacentElement('afterbegin', this.formElement)
+    }
+}
+
+const p = new Project()
