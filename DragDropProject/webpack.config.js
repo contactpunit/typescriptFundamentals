32a5254b6c039ccbe900eb5df363481c26ae72ed
Punit Jain <contactpunitjain@gmail.com>
2023-12-15 10:28:17 +0530

    added webpack config file

--- DragDropProject/webpack.config.js
+++ DragDropProject/webpack.config.js
@@ -1,1 +1,21 @@
-module.exports = {}
+const path = require('path')
+
+module.exports = {
+    entry: './src/app.ts',
+    output: {
+        path: path.resolve(__dirname, dist)
+    },
+    devtools: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    }
+}
