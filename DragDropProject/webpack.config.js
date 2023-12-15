93f95af4c5a31f852d1b4ca892a86f16edfcde4c
Punit Jain <contactpunitjain@gmail.com>
2023-12-15 13:20:23 +0530

    switch from live-server to webpack-dev-server and configuring webpack for development mode

--- DragDropProject/webpack.config.js
+++ DragDropProject/webpack.config.js
@@ -2,9 +2,18 @@ const path = require('path')
 
 module.exports = {
     entry: './src/app.ts',
+    mode: "development",
     output: {
         filename: 'app.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/dist/'
+    },
+    devServer: {
+        static: [
+          {
+            directory: path.join(__dirname),
+          },
+        ],
     },
     devtool: 'inline-source-map',
     module: {
